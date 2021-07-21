283aad93d9762761ce926d82b92db9cdf8d86fce
Dmitry Muravlev <dmur1991@gmail.com>
2021-07-21 17:32:11 +0300

    dz7

---
+++ Vertex.java
@@ -0,0 +1,16 @@
+package lesson4;
+
+import lombok.Data;
+
+@Data
+public class Vertex<T> {
+    private T payload;
+    private boolean visited;
+    private int weight;
+
+    public Vertex(T payload) {
+        this.payload = payload;
+        this.visited = false;
+        this.weight = -1;
+    }
+}
